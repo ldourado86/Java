9f47d4638aceacebd5cf53b68c5ba305309bc04d
Leandro Dourado <37157848+ldourado86@users.noreply.github.com>
2018-12-09 22:43:31 -0200

    Rename Aula04.java to DataLinguaEResolucaoSistema.java

--- DataLinguaEResolucaoSistema.java
+++ DataLinguaEResolucaoSistema.java
@@ -7,17 +7,17 @@ public class Aula04 {
 
 	public static void main(String[] args) {
       Date relogio = new Date();
-      System.out.println("A data do sistem È: ");
+      System.out.println("A data do sistem √©: ");
       System.err.println(relogio.toString());  
      
       Locale lingua = Locale.getDefault();
-      System.out.println("O seu sistema est· em " + lingua.getDisplayLanguage());
+      System.out.println("O seu sistema est√° em " + lingua.getDisplayLanguage());
       // OU
-      System.err.println("O seu sistema est· em " + lingua.getLanguage()); 
+      System.err.println("O seu sistema est√° em " + lingua.getLanguage()); 
       
       Toolkit tk = Toolkit.getDefaultToolkit();
       Dimension d = tk.getScreenSize();
-      System.out.println("A resoluÁ„o do seu sistema È:" + d.width + " X " + d.height);
+      System.out.println("A resolu√ß√£o do seu sistema √©:" + d.width + " X " + d.height);
       // OU
       System.out.println(tk.getScreenSize());
       
