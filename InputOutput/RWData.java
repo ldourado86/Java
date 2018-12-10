add9abe67387ce9c93f0582d7cc9a1c81b5ccb61
Leandro Dourado <37157848+ldourado86@users.noreply.github.com>
2018-12-09 22:34:56 -0200

    Add files via upload

---
+++ InputOutput/RWData.java
@@ -0,0 +1,60 @@
+
+import java.io.*;
+
+public class RWData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 10;
+		double d = 1023.56;
+		boolean b = true;
+		
+		try (DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("testdata"))) 
+		
+		{
+					
+			System.out.println("writing " + i);
+			dataOut.writeInt(i);
+			
+			System.out.println("writing " + d);
+			dataOut.writeDouble(d);
+			
+			System.out.println("writing " + b);
+			dataOut.writeBoolean(b);
+			
+			System.out.println("writing " + 12.2 * 7.4);
+			dataOut.writeDouble(12.2 * 7.4);
+		
+
+		} catch (IOException exc) {
+			System.out.println("write error");
+			return;
+		}
+		
+		System.out.println();
+		
+		
+		try (DataInputStream dataIn = new DataInputStream(new FileInputStream("testdata")))
+		
+		{
+						
+			i = dataIn.readInt();
+			System.out.println("Reading " + i);
+			
+			d = dataIn.readDouble();
+			System.out.println("Reading " + d);
+			
+			b = dataIn.readBoolean();
+			System.out.println("Reading " + b);
+			
+			d = dataIn.readDouble();
+			System.out.println("Reading " + d);
+		} 
+		catch (IOException exc) {
+			System.out.println("Read error");
+		}
+		
+	}
+
+}
